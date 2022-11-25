7064ebbce9902aa881111754bc4a63a993573dc4
Adam Wojnarowski <adam.piotr.wojnarowski@gmail.com>
2022-11-25 19:02:48 +0100

    fixing Pizza module

--- src/Pizza/Pizza.ts
+++ src/Pizza/Pizza.ts
@@ -1,31 +1,21 @@
 import { IIngredient } from "../Ingredient/IIngredient";
-import { Ingredients } from "../Ingredient/Ingredients";
 import { IPizza } from "./IPizza";
 
 export class Pizza implements IPizza {
+  name: string;
   ingredients: IIngredient[];
   price: number;
 
-  constructor() {
-    this.ingredients = [];
-    this.price = 0;
-  }
-
-  addIngredientToPizza(ingredient: IIngredient) {
-    this.ingredients.push(ingredient);
+  constructor(name: string, ingredients: IIngredient[]) {
+    this.name = name;
+    this.ingredients = [...ingredients];
     this.price = this.ingredients.reduce(
       (total, ingredient) => total + ingredient.price,
       0
     );
   }
 
-  removeIngredientFromPizza(ingredient: IIngredient) {
-    this.ingredients = this.ingredients.filter(
-      (ingredientFromList) => ingredientFromList.name !== ingredient.name
-    );
-    this.price = this.ingredients.reduce(
-      (total, ingredient) => total + ingredient.price,
-      0
-    );
+  addMargins(amount: number) {
+    this.price += amount;
   }
 }
