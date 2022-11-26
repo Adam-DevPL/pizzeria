2f3e167ac6dd660dc1a8d4b7e0ca49d434a13461
Adam Wojnarowski <adam.piotr.wojnarowski@gmail.com>
2022-11-26 18:56:52 +0100

    add Employee test (positive)

---
+++ test/employee.test.ts
@@ -0,0 +1,41 @@
+import "mocha";
+import { expect } from "chai";
+
+import { Employee } from "../src/Employee/Employee";
+
+describe("Employee", () => {
+  it("adding new employee", () => {
+    const employees = Employee.getInstance();
+    employees.addNewEmployee("Adam", "waiter");
+    const foundEmployee = employees.listOfEmployees.find(
+      (employee) => employee.name === "Adam"
+    );
+    expect(foundEmployee?.name).to.equal("Adam");
+  });
+  it("deleting employee", () => {
+    const employees = Employee.getInstance();
+    employees.addNewEmployee("Adam", "waiter");
+    employees.removeEmployee("Adam");
+    const foundEmployee = employees.listOfEmployees.find(
+      (employee) => employee.name === "Adam"
+    );
+    expect(foundEmployee).to.equal(undefined);
+  });
+  it("changing status of the employee", () => {
+    const employees = Employee.getInstance();
+    employees.addNewEmployee("Adam", "waiter");
+    employees.changeStatusOfEmployee("Adam");
+    const foundEmployee = employees.listOfEmployees.find(
+      (employee) => employee.name === "Adam"
+    );
+    expect(foundEmployee?.isFree).to.equal(false);
+  })
+  it("finding employee by the role", () => {
+    const employees = Employee.getInstance();
+    employees.addNewEmployee("Adam", "waiter");
+    employees.addNewEmployee("Dawid", "chef");
+
+    const foundEmployee = employees.findEmployee("chef");
+    expect(foundEmployee?.name).to.equal("Dawid");
+  })
+});
